f0b56745ff7a63efc55c41ff255e4dfc89d8348a
VikrantP16 <vikrantpatel1608@gmail.com>
2023-08-04 23:33:46 +0530

    new commit

--- colour_changer/app.js
+++ colour_changer/app.js
@@ -1,11 +1,4 @@
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
 
-buttons.forEach(function (button) {
-    console.log(button);
-    button.addEventListener('click', function(e){
-        console.log(e)
-        console.log(e.target)
-
-    })
-});
+function changeDate() {
+    document.getElementById("test").innerHTML = Date();
+}
