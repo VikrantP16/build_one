e06c0ea09578db3e6275935a9d1bde4e37a5eb2a
Vikrant Patel <90693378+VikrantP16@users.noreply.github.com>
2023-09-24 23:51:33 +0530

    directory update

--- lessons/test.js
+++
@@ -1,8 +0,0 @@
-// Note - Never use var due to issues in block scope and fucntional scope
-const restId = 92648
-let restName = 'demo resto'
-let password = 'user123'
-let restCity = 'vadodara'
-
-password = 'germanguy1989'
-console.table([restId, restName, password, restCity])
